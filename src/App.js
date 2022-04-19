c9a4e08f7d8dbbce59d265a927dd351f664594e7
khudoyor01 <75062167+khudoyor01@users.noreply.github.com>
2022-04-19 18:58:13 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainTable from "./Table/MainTable";
+
+function App() {
+  return (
+    <div className="App">
+      <MainTable />
+    </div>
+  );
+}
+
+export default App;
